e48ab5825128ba2b4dc722150cd3f573009d6301
Ching Lam Lau <contact.minicube11@gmail.com>
2025-06-21 20:11:35 -0400

    create prompting function

---
+++ prompt.py
@@ -0,0 +1,36 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GEMINI_API_KEY=os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+def get_response(prompt):
+    return client.models.generate_content(
+        model="gemini-2.0-flash", contents=prompt
+    ).text
+
+def generate_prompt(json, param, instruction):
+    prompt = f"""
+    You are an expert prompt engineer. You are given the following JSON:
+
+    {json}
+
+    Your task is:
+
+    Only update the value of "{param}" based on this instruction:
+    {instruction}
+
+    Do not modify any other lines, spacing, indentation, or trailing commas in the JSON.
+
+    Do not reorder keys or change formatting.
+
+    Do not reformat the JSON or adjust quotes.
+
+    Return the entire JSON with only that single change applied.
+    """
+
+    return prompt
